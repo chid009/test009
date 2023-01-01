b8dbf825b1831cc9cefb349f52249cf4f07dd370
chid009 <121409533+chid009@users.noreply.github.com>
2023-01-01 11:38:00 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  window.location="https://siriworldlove.xyz/2023/01/01/enjoy-every-day-with-the-best-video-loan-nguyen-2022-8015/?amp=1":
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
