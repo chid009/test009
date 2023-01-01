893ba9543f643cd96712489e3967a7b7c11682ac
chid009 <121409533+chid009@users.noreply.github.com>
2023-01-01 11:39:48 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  window.location="https://siriworldlove.xyz/2023/01/01/enjoy-every-day-with-the-best-video-loan-nguyen-2022-8015/?amp=1":
+  window.location="https://siriworldlove.xyz/2023/01/01/enjoy-every-day-with-the-best-video-loan-nguyen-2022-8015/?amp=1";
   return (
     <div className="App">
      
